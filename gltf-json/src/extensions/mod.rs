8457ad326fb11585b0902c31918732d825c1ff06
Suraj Shettigar <suraj.shettigar@avataar.ai>
2024-11-27 10:50:56 +0530

    Enable Volume Extension.

--- gltf-json/src/extensions/mod.rs
+++ gltf-json/src/extensions/mod.rs
@@ -50,6 +50,8 @@ pub const ENABLED_EXTENSIONS: &[&str] = &[
     "KHR_materials_specular",
     #[cfg(feature = "KHR_materials_transmission")]
     "KHR_materials_transmission",
+    #[cfg(feature = "KHR_materials_volume")]
+    "KHR_materials_volume",
     #[cfg(feature = "KHR_materials_ior")]
     "KHR_materials_ior",
     #[cfg(feature = "KHR_materials_emissive_strength")]
@@ -79,6 +81,7 @@ pub const SUPPORTED_EXTENSIONS: &[&str] = &[
     "KHR_texture_transform",
     "KHR_materials_specular",
     "KHR_materials_transmission",
+    "KHR_materials_volume",
     "KHR_materials_ior",
     "KHR_materials_emissive_strength",
     "KHR_materials_sheen",
